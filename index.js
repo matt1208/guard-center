01a8612fa1dee8b7b046b9eddbd726b2f9e292cd
Matthew <matthewvandyke08@gmail.com>
2022-11-01 15:34:11 -0700

    Form Styles + Responsive

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function formLogin() {
     let userpass = document.forms["login"]["pass"].value;
      if(userpass === pass) {
         // Display will make sure it doesn't still take up space on the page
-        document.getElementById("login").style.display = "none";
+        document.getElementById("userlogin").style.display = "none";
         document.getElementById("output").style.display = "block";
      } else {
-         alert("Wrong User Credentials. Please try again or contact an offical.");
+        alert("Wrong User Credentials. Please try again or contact an offical.");
      }
 }
