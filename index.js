cb43b821fc202c64c382e29aa39f831ac37be6bb
Matthew <matthewvandyke08@gmail.com>
2022-10-31 20:29:31 -0700

    First Test of Form Function

---
+++ index.js
@@ -0,0 +1,11 @@
+function formLogin() {
+    var pass = "bls";
+    let userpass = document.forms["login"]["pass"].value;
+     if(userpass === pass) {
+        // Display will make sure it doesn't still take up space on the page
+        document.getElementById("login").style.display = "none";
+        document.getElementById("output").style.display = "block";
+     } else {
+         alert("Wrong User Credentials. Please try again or contact an offical.");
+     }
+}
