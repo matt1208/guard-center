82118cfca9179edff1de971c54432ece1a34d67d
Matthew <matthewvandyke08@gmail.com>
2022-11-02 22:05:00 -0700

    Number Tracking Form. No function only content

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function formLogin() {
     let userpass = document.forms["login"]["pass"].value;
      if(userpass === pass) {
         // Display will make sure it doesn't still take up space on the page
-        document.getElementById("userlogin").style.display = "none";
-        document.getElementById("output").style.display = "block";
+        document.getElementById("initalStage").style.display = "none";
+        document.getElementById("maincontent").style.display = "block";
      } else {
         alert("Wrong User Credentials. Please try again or contact an offical.");
      }
