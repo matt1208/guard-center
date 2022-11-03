a8bec065084d46036c293856168394b512302b38
Matthew <matthewvandyke08@gmail.com>
2022-11-03 15:55:55 -0700

    Google Sheets Function with Google App Script

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function formLogin() {
         alert("Wrong User Credentials. Please try again or contact an offical.");
      }
 }
+
+window.addEventListener("load", function() {
+   const form = document.getElementById('numberform');
+   form.addEventListener("submit", function(e) {
+       e.preventDefault();
+       const data = new FormData(form);
+       const action = e.target.action;
+       fetch(action, {
+       method: 'POST',
+       body: data,
+       })
+       .then(() => {
+       alert("Success!");
+       })
+   });
+   });
