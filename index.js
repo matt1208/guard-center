cf26bcbfa233e6311fbe20f7fb2e8a1df70b2470
Matthew <matthewvandyke08@gmail.com>
2023-05-26 22:57:51 -0700

    new staff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function formLogin() {
     var pass = "bls";
-    var users = ["blsadmin", "mattvd", "eviep", "kennyc", "masonj", "charlied", "jackd"];
+    var users = ["blsadmin", "mattvd", "eviep", "kennyc", "masonj", "charlied", "jackd", "carlieg"];
     let userpass = document.forms["login"]["pass"].value;
     let username = document.forms["login"]["username"].value;
      if(userpass === pass && users.includes(username)) {
